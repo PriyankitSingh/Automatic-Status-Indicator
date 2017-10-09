ad5c86f7cd99756b5475deabb29ae0bc77141f3e
PriyankitSingh <psin778@aucklanduni.ac.nz>
2017-10-09 22:58:01 +1300

    Added firebase access from background script

--- background.js
+++ background.js
@@ -1,19 +1,59 @@
 /**
  * Created by Jay on 10/4/2017.
  */
+ // important properties
+ var state = "available";
+
 chrome.idle.onStateChanged.addListener(function(newstate) {
+    state = newstate;
     console.log(newstate);
     // history_log.unshift({'state':newstate, 'time':time})
 });
 
 chrome.browserAction.onClicked.addListener(function() {
-    window.open('popup.html', 'testwindow', 'width=700,height=600');
+    window.open('index.html', 'testwindow', 'width=700,height=600');
 });
 
 // Initialize Firebase
-var config = {
-   apiKey: "<qwertyuiopasdfghjklzxcvbnm>", // change this
-   databaseURL: "https://<my-app-id>.firebaseio.com",
-   storageBucket: "<my-app-id>.appspot.com"
-};
-firebase.initializeApp(config);
+  var config = {
+    apiKey: "AIzaSyAEv8m64fZy6wROGL-zkMcQkAB-mcdKn6U",
+    authDomain: "automatic-status-indicator.firebaseapp.com",
+    databaseURL: "https://automatic-status-indicator.firebaseio.com",
+    projectId: "automatic-status-indicator",
+    storageBucket: "automatic-status-indicator.appspot.com",
+    messagingSenderId: "1076122007504"
+  };
+  firebase.initializeApp(config);
+
+
+  chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
+    if (request.greeting == "hello"){
+      sendResponse({farewell: "goodbye"});
+    }
+    if(request.info == "database"){
+    	var userRef = firebase.database();
+    	console.log(userRef);
+    	sendResponse({data: userRef});
+    }
+    if (request.info == "state"){
+    	console.log(state);
+    	sendResponse({data: state});
+    }
+    if (request.add.length == 2){
+    	console.log(request.add[0] + " " + request.add[1]);
+    	addToDatabase(request.add[0], request.add[1]);
+    	sendResponse({status: "done"});
+    }
+  });
+
+function addToDatabase(username, userstatus){
+	var allUsers = firebase.database().ref("users");
+	allUsers.push({
+	  	name: username,
+	  	status: userstatus
+  });
+}
--- index.js
+++ index.js
@@ -1,44 +1,61 @@
-// Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyAEv8m64fZy6wROGL-zkMcQkAB-mcdKn6U",
-    authDomain: "automatic-status-indicator.firebaseapp.com",
-    databaseURL: "https://automatic-status-indicator.firebaseio.com",
-    projectId: "automatic-status-indicator",
-    storageBucket: "automatic-status-indicator.appspot.com",
-    messagingSenderId: "1076122007504"
-  };
-  firebase.initializeApp(config);
-  console.log(firebase)
 
-  var ref = firebase.database().ref(0);
-  console.log(ref);
-
-  var userRef = firebase.database().ref("users/0");
-  console.log(userRef);
-  userRef.update({
-  	"name": "Jay"
-  })
-
-  var allUsers = firebase.database().ref("users");
-  console.log(allUsers);
-  // allUsers.set({
-  // 	name: "John Doe",
-  // 	status: "available"
-  // });
-
-  // Retrieve new posts as they are added to our database
-allUsers.on("child_added", function(snapshot, prevChildKey) {
-  var newPost = snapshot.val();
-  console.log("Name: " + newPost.name);
-  console.log("Status: " + newPost.status);
+chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
+  console.log(response.farewell);
 });
 
-allUsers.push({
-  	name: "Jane Doe",
-  	status: "unavailable"
-  });
+chrome.runtime.sendMessage({info: "database"}, function(response) {
+  console.log(response.data);
+});
 
-allUsers.child("Jay").set({
-    "name": "Jay Pandya",
-    "status": "active"
-})
+chrome.runtime.sendMessage({info: "state"}, function(response) {
+  console.log(response.data);
+});
+
+chrome.runtime.sendMessage({add: ["Priyankit Singh", "available"]}, function(response) {
+  console.log(response.status);
+});
+
+// // Initialize Firebase
+//   var config = {
+//     apiKey: "AIzaSyAEv8m64fZy6wROGL-zkMcQkAB-mcdKn6U",
+//     authDomain: "automatic-status-indicator.firebaseapp.com",
+//     databaseURL: "https://automatic-status-indicator.firebaseio.com",
+//     projectId: "automatic-status-indicator",
+//     storageBucket: "automatic-status-indicator.appspot.com",
+//     messagingSenderId: "1076122007504"
+//   };
+//   firebase.initializeApp(config);
+//   console.log(firebase)
+
+//   var ref = firebase.database().ref(0);
+//   console.log(ref);
+
+//   var userRef = firebase.database().ref("users/0");
+//   console.log(userRef);
+//   userRef.update({
+//   	"name": "Jay"
+//   })
+
+//   var allUsers = firebase.database().ref("users");
+//   console.log(allUsers);
+//   // allUsers.set({
+//   // 	name: "John Doe",
+//   // 	status: "available"
+//   // });
+
+//   // Retrieve new posts as they are added to our database
+// allUsers.on("child_added", function(snapshot, prevChildKey) {
+//   var newPost = snapshot.val();
+//   console.log("Name: " + newPost.name);
+//   console.log("Status: " + newPost.status);
+// });
+
+// allUsers.push({
+//   	name: "Jane Doe",
+//   	status: "unavailable"
+//   });
+
+// allUsers.child("Jay").set({
+//     "name": "Jay Pandya",
+//     "status": "active"
+// })
